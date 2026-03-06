2c3644c9428ce49b0342d8912dc622fefdfdb465
Diego Iturrieta Gajardo <110798454+SanDBIG@users.noreply.github.com>
2026-03-05 22:44:55 -0300

    Setup
    Se crea el script de libraries para la creación y organización de las librerias a usar.

---
+++ 02_scripts/01_libraries.R
@@ -0,0 +1,21 @@
+# =====================================================
+# 1. CARGA DE LIBRERIAS
+# =====================================================
+
+install.packages("pacman")
+
+# Cargar librerías
+pacman::p_load(
+  tidyverse,   # Manipulación y visualización (dplyr, ggplot2, etc.)
+  janitor,     # Limpieza de nombres y tablas
+  skimr,       # Exploración rápida de datos
+  lubridate,   # Manejo de fechas
+  readxl,      # Importar Excel
+  writexl,     # Exportar Excel
+  data.table,  # Manipulación rápida de grandes datasets
+  scales,      # Escalas para gráficos
+  patchwork,   # Combinar gráficos ggplot
+  ggthemes     # Temas adicionales para gráficos
+)
+
+
