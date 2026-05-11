531241b359763addee5f2407356f61bad517dd4a
Diego Iturrieta Gajardo <110798454+SanDBIG@users.noreply.github.com>
2026-05-11 18:06:39 -0400

    Wrangling
    actualizacion de main con source de graficos y tablas

--- main.R
+++ main.R
@@ -6,6 +6,7 @@ source("02_R/02_load_data.R")
 source("02_R/03_clean_data.R") 
 # Exploratory data analysis all variables
 source("02_R/04_eda.R")
-# 
-source("02_R/")
-
+# Creacion de tablas y graficos estaticos
+source("02_R/06_graphs-visuals.R")
+# Creacion de dashboard shiny
+source("02_R/Dashboard.R")
