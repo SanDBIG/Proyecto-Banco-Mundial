f2dce542eedff172e99fe964a8b1b12854dbce9b
Diego Iturrieta Gajardo <110798454+SanDBIG@users.noreply.github.com>
2026-05-11 17:50:22 -0400

    Wrangling
    acomodo de los scripts construidos anteriormente

--- 02_R/03_clean_data.R
+++ 02_R/03_clean_data.R
@@ -48,6 +48,76 @@ data_clean <- data_clean %>%
 #sapply(data_clean, class) #Comprobar si fueron transformadas
 
 
+# Transformación de nombres de paises -------------------------------------
+
+# TRANSFORMACIÓN DE NOMBRES DE PAÍSES -----------------------------------------
+
+data_clean <- data_clean %>%
+  mutate(
+    `Country Name` = case_when(
+      `Country Name` == "Antigua and Barbuda" ~ "Antigua_and_Barbuda",
+      `Country Name` == "Bahamas, The" ~ "Bahamas_The",
+      `Country Name` == "Bosnia and Herzegovina" ~ "Bosnia_and_Herzegovina",
+      `Country Name` == "Brunei Darussalam" ~ "Brunei_Darussalam",
+      `Country Name` == "Burkina Faso" ~ "Burkina_Faso",
+      `Country Name` == "Central African Republic" ~ "Central_African_Republic",
+      `Country Name` == "Congo, Dem. Rep." ~ "Congo_Dem_Rep",
+      `Country Name` == "Congo, Rep." ~ "Congo_Rep",
+      `Country Name` == "Costa Rica" ~ "Costa_Rica",
+      `Country Name` == "C\xf4te d'Ivoire" ~ "Cote_d_Ivoire",
+      `Country Name` == "Czech Republic" ~ "Czech_Republic",
+      `Country Name` == "Dominican Republic" ~ "Dominican_Republic",
+      `Country Name` == "Egypt, Arab Rep." ~ "Egypt_Arab_Rep",
+      `Country Name` == "El Salvador" ~ "El_Salvador",
+      `Country Name` == "Equatorial Guinea" ~ "Equatorial_Guinea",
+      `Country Name` == "Gambia, The" ~ "Gambia_The",
+      `Country Name` == "Guinea-Bissau" ~ "Guinea_Bissau",
+      `Country Name` == "Iran, Islamic Rep." ~ "Iran_Islamic_Rep",
+      `Country Name` == "Korea, Rep." ~ "Korea_Rep",
+      `Country Name` == "Kyrgyz Republic" ~ "Kyrgyz_Republic",
+      `Country Name` == "Lao PDR" ~ "Lao_PDR",
+      `Country Name` == "Marshall Islands" ~ "Marshall_Islands",
+      `Country Name` == "Micronesia, Fed. Sts." ~ "Micronesia_Fed_Sts",
+      `Country Name` == "New Zealand" ~ "New_Zealand",
+      `Country Name` == "North Macedonia" ~ "North_Macedonia",
+      `Country Name` == "Papua New Guinea" ~ "Papua_New_Guinea",
+      `Country Name` == "Puerto Rico" ~ "Puerto_Rico",
+      `Country Name` == "Russian Federation" ~ "Russian_Federation",
+      `Country Name` == "San Marino" ~ "San_Marino",
+      `Country Name` == "S\xe3o Tom\xe9 and Principe" ~ "Sao_Tome_and_Principe",
+      `Country Name` == "Saudi Arabia" ~ "Saudi_Arabia",
+      `Country Name` == "Sierra Leone" ~ "Sierra_Leone",
+      `Country Name` == "Slovak Republic" ~ "Slovak_Republic",
+      `Country Name` == "Solomon Islands" ~ "Solomon_Islands",
+      `Country Name` == "South Africa" ~ "South_Africa",
+      `Country Name` == "South Sudan" ~ "South_Sudan",
+      `Country Name` == "Sri Lanka" ~ "Sri_Lanka",
+      `Country Name` == "St. Kitts and Nevis" ~ "St_Kitts_and_Nevis",
+      `Country Name` == "St. Lucia" ~ "St_Lucia",
+      `Country Name` == "St. Vincent and the Grenadines" ~ "St_Vincent_and_the_Grenadines",
+      `Country Name` == "Syrian Arab Republic" ~ "Syrian_Arab_Republic",
+      `Country Name` == "Taiwan, China" ~ "Taiwan_China",
+      `Country Name` == "Timor-Leste" ~ "Timor_Leste",
+      `Country Name` == "Trinidad and Tobago" ~ "Trinidad_and_Tobago",
+      `Country Name` == "United Arab Emirates" ~ "United_Arab_Emirates",
+      `Country Name` == "United Kingdom" ~ "United_Kingdom",
+      `Country Name` == "United States" ~ "United_States",
+      `Country Name` == "Venezuela, RB" ~ "Venezuela_RB",
+      `Country Name` == "West Bank and Gaza" ~ "West_Bank_and_Gaza",
+      `Country Name` == "Yemen, Rep." ~ "Yemen_Rep",
+      
+      `Country Name` == "Hong Kong, China" ~ "Hong_Kong_China",
+      `Country Name` == "Los Angeles" ~ "Los_Angeles",
+      `Country Name` == "Mexico City" ~ "Mexico_City",
+      `Country Name` == "New York" ~ "New_York",
+      `Country Name` == "Rio de Janeiro" ~ "Rio_de_Janeiro",
+      `Country Name` == "Sao Paulo" ~ "Sao_Paulo",
+      `Country Name` == "St. Petersburg" ~ "St_Petersburg",
+      
+      TRUE ~ `Country Name`
+    )
+  )
+
 # NOTA TÉCNICA ------------------------------------------------------------
 
 # Para construir dashboards los datos deben ir en formato long, esto para que los
--- 02_R/05_countries_cities.R
+++ 02_R/05_countries_cities.R
@@ -1,26 +1,37 @@
+########################################################################
+# Se pasa todo este codigo a clean data para facilitar nombres de paises  --------
+########################################################################
+
+
+
 
 # Paises ------------------------------------------------------------------
 
 # unique(data_raw$`Country Name`)
 
-paises <- split(data_raw, data_raw$`Country Name`)
-
-# exploración de la estructura de la lista
-str(paises) # estructura general de la lista
-names(paises) # nombres de los elementos
-length(paises) # cantidad de elementos
-head(paises) # ver primeros 5 elementos
-summary(paises) # resumen de la lista
-sapply(paises, class) # Tipo de cada elemento de la lista
-sapply(paises, dim) # dimensiones si contiene df's, matrices u otros objetos tabulares
-sapply(paises, nrow) # número de filas
-sapply(paises, ncol) # número de columnas
-
-lapply(paises, names) # ver nombre de las columnas decada DF
-lapply(paises, head) # ver prifmeras filas de cada DF
-lapply(paises, str) # vere structurar de cada DF
-
-
+# se corrige que paises debe partir de data_clean
+# se corrige para arrancar como df y no como lista
+# 
+# paises <- data_clean %>% 
+#   group_by(`Country Name`) #para agrugar sin crear variables se usa backticks ``
+# 
+# names(paises)
+# 
+# # exploración de la estructura de DF
+# str(paises) # estructura general de la df
+# names(paises) # nombres de los elementos
+# length(paises) # cantidad de elementos
+# head(paises) # ver primeros 5 elementos
+# summary(paises) # resumen de la lista
+# sapply(paises, class) # Tipo de cada elemento de la lista
+# sapply(paises, dim) # dimensiones si contiene df's, matrices u otros objetos tabulares
+# sapply(paises, nrow) # número de filas
+# sapply(paises, ncol) # número de columnas
+# 
+# lapply(paises, names) # ver nombre de las columnas decada DF
+# lapply(paises, head) # ver prifmeras filas de cada DF
+# lapply(paises, str) # ver estructura de cada DF
+# 
 
 # Afghanistan <- paises$Afghanistan
 # Albania <- paises$Albania
