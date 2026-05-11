23cfb87439b76544c3fe1181044d4003c8e181f0
Diego Iturrieta Gajardo <110798454+SanDBIG@users.noreply.github.com>
2026-05-11 17:51:06 -0400

    Visuals
    Construccion de tablas bajo parametros de interes

--- 02_R/06_graphs-visuals.R
+++ 02_R/06_graphs-visuals.R
@@ -1,8 +1,46 @@
 
+# Se toman los anios con >80 cobertura ------------------------------------
+
+umbral_anios <- ceiling(0.80 * length(2003:2019))
+
+
+# Cobertura por país e indicador ---------------------------------------------
+
+cobertura_pais_indicador <- data_clean_long %>%
+  filter(Year >= 2003, Year <= 2019) %>%
+  group_by(`Country Name`, 
+           `Country Code`, 
+           `Series Name`, 
+           `Series Code`) %>%
+  summarise(anios_con_dato = n_distinct(Year[!is.na(Value)]),
+    cobertura = anios_con_dato / length(2003:2019),
+    .groups = "drop"
+  )
+
+
+# Filtrar país-indicador con cobertura >= 80% --------------------------------
+
+data_80cob <- data_clean_long %>%
+  filter(Year >= 2003, Year <= 2019) %>%
+  group_by(`Country Name`, 
+           `Country Code`, 
+           `Series Name`, 
+           `Series Code`) %>%
+  mutate(
+    anios_con_dato = n_distinct(Year[!is.na(Value)]),
+    cobertura = anios_con_dato / length(2003:2019)
+  ) %>%
+  filter(anios_con_dato >= ceiling(0.80 * length(2003:2019))) %>%
+  ungroup()
+
+# unique(data_80cob$`Country Name`)
+
+
 # Tablas ------------------------------------------------------------------
+# Paises del cono sur
 
-
-
+data_conosur <- data_80cob %>%
+  filter(`Country Name` %in% c("Chile", "Argentina", "Uruguay"))
 
 
 
