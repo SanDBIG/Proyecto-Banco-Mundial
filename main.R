dedcf90d2050ab88c851736e02f15734bb6263cc
Diego Iturrieta Gajardo <diturrieta.g@gmail.com>
2026-05-11 00:15:13 -0400

    Wrangling
    Organización de directorios y delimitación para dashboard

--- main.R
+++ main.R
@@ -1,5 +1,11 @@
-source("02_R/01_libraries.R") # Carga de librerias
-source("02_R/02_load_data.R") # Carga de datos
-source("02_R/03_clean_data.R") # Data limpia y creación de formato long
-source("02_R/04_eda.R") # Exploratory data analysis
+# Carga de librerias
+source("02_R/01_libraries.R")
+# Carga de datos
+source("02_R/02_load_data.R") 
+# Data limpia y creación de formato long
+source("02_R/03_clean_data.R") 
+# Exploratory data analysis all variables
+source("02_R/04_eda.R")
+# 
 source("02_R/")
+
